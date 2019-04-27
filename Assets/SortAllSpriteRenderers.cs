803187876aa26e81a6e8a1d914086f90a65705ed
Josh Maggard <jmaggard62@gmail.com>
2019-04-27 02:32:50 -0500

    Fix shadow sort-ish

--- Assets/SortAllSpriteRenderers.cs
+++ Assets/SortAllSpriteRenderers.cs
@@ -1,13 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class SortAllSpriteRenderers : MonoBehaviour
 {
     // Update is called once per frame
     void Update()
     {
-        foreach (var c in FindObjectsOfType<TopDownController>())
-            c.GetComponent<SpriteRenderer>().sortingOrder = Mathf.CeilToInt(-c.transform.position.y * 100) - 300;
+        foreach (var c in FindObjectsOfType<SpriteRenderer>())
+            c.sortingOrder = Mathf.CeilToInt((-c.transform.position.y - 300) * 100);
     }
 }
