0882b19250d89080f70160446e155e054ec4a9d7
Josh Maggard <jmaggard62@gmail.com>
2019-04-26 23:39:58 -0500

    Basic 2d stuff

---
+++ Assets/Character/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(TopDownController))]
+public class PlayerController : MonoBehaviour
+{
+    private TopDownController controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<TopDownController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        controller.xMove = Input.GetAxisRaw("Horizontal");
+        controller.yMove = Input.GetAxisRaw("Vertical");
+    }
+}
---
+++ Assets/Character/TopDownController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class TopDownController : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+    private Animator animator;
+
+    public float xMove;
+    public float yMove;
+
+    public float speed = 2;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void LateUpdate()
+    {
+        if (Mathf.Abs(xMove) > 0 || Mathf.Abs(yMove) > 0)
+        {
+            animator.SetFloat("lastXMove", xMove);
+            animator.SetFloat("lastYMove", yMove);
+            rigidBody.MovePosition((Vector2)transform.position + new Vector2(xMove, yMove) * speed * Time.deltaTime);
+        }
+        animator.SetFloat("xMove", xMove);
+        animator.SetFloat("yMove", yMove);
+        xMove = 0;
+        yMove = 0;
+    }
+}
