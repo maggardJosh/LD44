864f998a910da6b9be7a38c09c2ff3ce00075cce
Dank <danves12@gmail.com>
2019-04-28 02:10:21 -0500

    Beginning to add sound management

---
+++ Assets/Audio/MusicTransition.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MusicTransition : MonoBehaviour
+{
+    private static MusicTransition _instance;
+    public static MusicTransition Instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = FindObjectOfType<MusicTransition>();
+            return _instance;
+        }
+    }
+
+    
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(_instance);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Audio/SoundManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundManager : MonoBehaviour
+{
+    private static SoundManager _instance;
+    public static SoundManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = FindObjectOfType<SoundManager>();
+            return _instance;
+        }
+    }
+
+    public void PlayMusic(GameObject objectToPlay, AudioClip clipToPlay)
+    {
+        objectToPlay.GetComponent<AudioSource>().PlayOneShot(clipToPlay);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
