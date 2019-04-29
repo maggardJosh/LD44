93a5b35883676ad36b6a2b3d77cadcc8a5111108
Josh Maggard <jmaggard62@gmail.com>
2019-04-29 18:30:56 -0500

    Boss memory started

---
+++ Assets/MemoryLoadOnCollide.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MemoryLoadOnCollide : MonoBehaviour
+{
+    public string MemoryScene = "";
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !string.IsNullOrWhiteSpace(MemoryScene))
+        {
+            PlayerController p = collision.GetComponent<PlayerController>();
+            p.SaveScenePositionForMemory();
+            SoundManager.Instance.PlaySound(SoundManager.Sound.Music_Transition1);
+            FadeTransitionScreen.Instance.Transition(() =>
+            {
+                SceneManager.LoadScene(MemoryScene);
+            });
+        }
+    }
+}
